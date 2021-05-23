8e29f5d76b4a3f47c8d3bf6e2c366760da95c715
ido <ekabelly@gmail.com>
2021-05-23 18:22:28 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-cd ./gallery-client
-npm i
-cd ../gallery-api
-npm i
-node app.js
+(cd ./gallery-api; npm i; node app.js) &
+(cd ./gallery-client; npm i; npm run build; npm install -g serve; serve -s build)
